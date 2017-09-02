fcb77ba1e3bfba1ba16eff5791b4a549ab9cf886
Can Chen <canchen@me.com>
2017-09-02 08:40:23 -0400

    Implement the API to connect to the back-end server API
    All API tested.
    
    TODO: 
    1. Remove the testing code

--- front-end/src/components/Article.js
+++ front-end/src/components/Article.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { getAllCategories } from '../utils/API'
+import { getAllPosts } from '../utils/API'
+import { getAllPostsInCategory } from '../utils/API'
+import { getPost } from '../utils/API'
+import { getALLCommentsOfPost } from '../utils/API'
+import { getComment } from '../utils/API'
+import { addNewPost } from '../utils/API'
+import { votePost } from '../utils/API'
+import { editPost } from '../utils/API'
+import { deletePost } from '../utils/API'
+import { addCommentToPost } from '../utils/API'
+import { voteComment } from '../utils/API'
+import { editComment } from '../utils/API'
+import { deleteComment } from '../utils/API'
 
 class Article extends Component {
+
+	componentDidMount() {
+		console.log(getAllCategories())
+		console.log(getAllPosts())
+		console.log(getAllPostsInCategory('redux'))
+		console.log(getPost("6ni6ok3ym7mf1p33lnez"))
+		console.log(getALLCommentsOfPost("894tuq4ut84ut8v4t8wun89g"))
+		console.log(getComment("321tuq4ut84ut8v4t8wun89g"))
+		console.log(addNewPost( "333tuq4ut84ut8v4t8wun89l", Date.now(), "myTitle", "this is my body", "can can", "redux" ))
+		console.log(votePost("333tuq4ut84ut8v4t8wun89l", "upVote"))
+		console.log(editPost("333tuq4ut84ut8v4t8wun89l", "newTitle", "shaped!"))
+		console.log(deletePost("8xf0y6ziyjabvozdd253nd"))
+		console.log(addCommentToPost( "321tuq4ut84ut8v4t8wun89g", Date.now(), "my body is here", "can can", "894tuq4ut84ut8v4t8wun89g" ))
+		console.log(voteComment("321tuq4ut84ut8v4t8wun89g", "upVote"))
+		console.log(editComment("321tuq4ut84ut8v4t8wun89g", Date.now(), "this new body"))
+		console.log(deleteComment("321tuq4ut84ut8v4t8wun89g"))
+	}
 
 	render () {
 		return (
