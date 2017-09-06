8f79d1ffbfba9fc931cb28de4a9a52b1285920b5
Can Chen <canchen@me.com>
2017-09-06 11:18:20 -0400

    Hide Reply Panel if no post is active
    TODO: change the logic to hide chatArea and show help is no post is active

--- front-end/src/components/replyArea/ReplyPanel.js
+++ front-end/src/components/replyArea/ReplyPanel.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import ReplyPanelActionBar from './ReplyPanelActionBar'
 import ReplyPanelInputArea from './ReplyPanelInputArea'
 import ReplyPanelSubmitButton from './ReplyPanelSubmitButton'
 
 class ReplyPanel extends Component {
 
+	replyPanel () {
+		if (this.props.post === undefined) {
+			return (
+				<div>
+				</div>
+			)
+		}
+		else {
+			return (
+				<div className="reply-wraper">
+					<ReplyPanelActionBar
+					></ReplyPanelActionBar>
+					<ReplyPanelInputArea
+					></ReplyPanelInputArea>
+					<ReplyPanelSubmitButton
+					></ReplyPanelSubmitButton>
+				</div>
+			)			
+		}
+	}
+
 	render () {
 		return (
-			<div className="reply-wraper">
-				<ReplyPanelActionBar
-				></ReplyPanelActionBar>
-				<ReplyPanelInputArea
-				></ReplyPanelInputArea>
-				<ReplyPanelSubmitButton
-				></ReplyPanelSubmitButton>
-			</div>
+			this.replyPanel()
 		)
 	}
 }
 
-export default ReplyPanel
+function mapStateToProps ({ activePost }) {
+	return {
+		post: activePost.post,
+	}
+}
+
+export default connect(mapStateToProps)(ReplyPanel)
